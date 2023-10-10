55cd47a3936b33f4ecbecd7aabaa9367acb0fe6e
tmtabor <tmtabor@cloud.ucsd.edu>
2023-10-10 14:02:34 -0700

    Version fix

--- jupyter-wysiwyg/__init__.py
+++ jupyter-wysiwyg/__init__.py
@@ -1,16 +1,8 @@
-import json
-from pathlib import Path
+__version__ = "23.10.0"
 
-from ._version import __version__
-
-HERE = Path(__file__).parent.resolve()
-
-with (HERE / "labextension" / "package.json").open() as fid:
-    data = json.load(fid)
 
 def _jupyter_labextension_paths():
     return [{
         "src": "labextension",
-        "dest": data["name"]
+        "dest": "@g2nb/jupyter-wysiwyg"
     }]
-
